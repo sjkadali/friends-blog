cd40c06bad3ba069fa8719f03ca88b6b51f56fc1
sjkadali <skadalikat@gmail.com>
2024-05-10 18:10:54 -0400

    Added Post feature

--- friends-blog/src/components/categoryList/CategoryList.jsx
+++ friends-blog/src/components/categoryList/CategoryList.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import styles from './categoryList.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export const CategoryList = () => {
   return (
-    <div className={styles.container}>CategoryList</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <div className={styles.category}>        
+          <Link  href="/blog?cat=style"
+            className={`${styles.category} ${styles.food}`}>
+            <Image 
+              src="/fries.jpg"
+              alt=""
+              width={32}
+              height={32}
+              className={styles.Image}
+            />
+            Food
+          </Link>
+          <Link  href="/blog?cat=style"
+            className={`${styles.category} ${styles.travel}`}>
+            <Image 
+              src="/travel.jpg"
+              alt=""
+              width={32}
+              height={32}
+              className={styles.Image}
+            />
+            Travel
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
--- friends-blog/src/components/featured/Featured.jsx
+++ friends-blog/src/components/featured/Featured.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import styles from './featured.module.css'
+import Image from 'next/image'
 
 export const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <p className={styles.title}>
+        <b>Welcome to Friendly blog!</b>Explore the blog stories.
+      </p>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/floweringCherry.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet alim consectetur adipisicing elit.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Cupiditate, quam nisi magni ea laborum inventore voluptatum
+            laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium
+            quisquam! Harum unde sit culpa debitis.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
+
